2b6f211e41c57aba7813bc156509fba141fd9c77
Dominik Conway <dominik.conway@gmail.com>
2022-06-04 16:05:27 -0700

    full crud

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,5 +1,4 @@
 import {useState} from 'react'
-import {Link} from 'react-router-dom'
 import Show from '../components/Show'
 
 export default function Index ({bookmarks, createBookmarks, updateBookmarks, deleteBookmarks}) {
